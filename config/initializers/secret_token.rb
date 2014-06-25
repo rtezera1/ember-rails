daf250ff3920dfd53df0debf200bdff324b8e5f3
Robel <robeltadesse01@gmail.com>
2014-06-25 13:31:38 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EmberRails::Application.config.secret_key_base = '9e0b1254e152fd173c752c1505678491445ccab2ec31e7a8c1b5cbebf7c06b6e5e003178bfe7c35b723c18329a5bcd614fdb7cc56c59aa4954ac8b21b80488a8'
